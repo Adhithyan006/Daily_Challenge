d463894a54081eacf5758387191e1a65b5cb9b1d
Adhithyan006 <kit27.am02@gmail.com>
2025-12-30 21:55:03 +0530

    Add files via upload

---
+++ day12(c).py
@@ -0,0 +1,48 @@
+# Leetcode Qn no: 47
+# Qn name: Permutations II
+# Given a collection of numbers, nums, that might contain duplicates,
+# return all possible unique permutations in any order.
+
+ 
+
+# Example 1:
+# Input: nums = [1,1,2]
+# Output:
+# [[1,1,2],
+#  [1,2,1],
+#  [2,1,1]]
+
+# Example 2:
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ 
+# Constraints:
+# 1 <= nums.length <= 8
+# -10 <= nums[i] <= 10
+
+#MY SOLUTION
+from typing import List
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        used = [False] * len(nums)
+        
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+                    continue
+                used[i] = True
+                path.append(nums[i])
+                backtrack(path)
+                path.pop()
+                used[i] = False
+        
+        backtrack([])
+        return res
