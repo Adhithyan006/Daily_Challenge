dc5874abbfc8c5e441d9827e26c97ab42bd41937
ADHITHYAN M <kit27.am02@gmail.com>
2026-01-20 22:01:09 +0530

    Add files via upload

---
+++ day30(c).py
@@ -0,0 +1,49 @@
+# Hackerrank Qn: Re.start() & Re.end()
+# These expressions return the indices of the start and end of the substring matched by the group.
+
+# Code
+# >>> import re
+# >>> m = re.search(r'\d+','1234')
+# >>> m.end()
+# 4
+# >>> m.start()
+# 0
+# Task
+# You are given a string .
+# Your task is to find the indices of the start and end of string  in .
+
+# Input Format
+# The first line contains the string .
+# The second line contains the string .
+
+# Constraints
+# Output Format
+# Print the tuple in this format: (start _index, end _index).
+# If no match is found, print (-1, -1).
+
+# Sample Input
+
+# aaadaa
+# aa
+# Sample Output
+
+# (0, 1)  
+# (1, 2)
+# (4, 5)
+
+# MY SOLUTION 
+import re
+
+s = input().strip()
+sub = input().strip()
+
+pattern = re.compile(f'(?={sub})')
+matches = pattern.finditer(s)
+
+found = False
+for m in matches:
+    print((m.start(), m.start() + len(sub) - 1))
+    found = True
+
+if not found:
+    print((-1, -1))
